0e7fccf126b5bb004a9521632e6dc73d03f09881
AMIRSOHAIL1998 <amir@betalectic.com>
2023-03-02 17:30:17 +0530

    pushing server and db files

---
+++ connection.js
@@ -0,0 +1,22 @@
+const Sequelize = require("sequelize");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const sequelize = new Sequelize(
+  process.env.PGDB,
+  process.env.PGUSER,
+  process.env.PGPASSWORD,
+  {
+    host: process.env.PGHOST,
+    dialect: process.env.PGDIALECT,
+  }
+);
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((error) => {
+    console.error("Unable to connect to the database: ", error);
+  });
